90d63921b4b5d5108804815e5b55b501e1637968
DamjanVincic <damjanvincic2003@gmail.com>
2026-02-03 01:13:54 +0100

    feat: actuators data publish

--- simulation/components/db.py
+++ simulation/components/db.py
@@ -1,5 +1,10 @@
+from mqtt_publisher import send_measurement
+
+
 def toggle_buzzer(settings, state):
     if settings['simulated']:
         print(f"SIMULATOR: Door Buzzer is now {'ON' if state else 'OFF'}")
+        topic = settings['topic']
+        send_measurement(topic, 1 if state else 0, "DB", settings['device'], is_simulated=settings['simulated'])
     else:
         pass #TODO : implement real actuator logic
