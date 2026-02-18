727032f0cbb319360526b00f93eab4dc7ff0d362
Mijat Krivokapić <mijat2004@gmail.com>
2026-02-18 14:53:24 +0100

    Implement pi2 mode

--- simulation/main.py
+++ simulation/main.py
@@ -10,6 +10,7 @@ from components.ds import run_ds
 from components.dus import run_dus
 from components.ir import run_ir
 from components.pir import run_pir
+from components.gyro import run_gyro
 from components.actuator_callback import actuator_callback
 from mqtt_wrapper import init_mqtt_client, get_mqtt_client
 from settings import load_settings
@@ -99,12 +100,17 @@ if __name__ == "__main__":
                     run_ds(pi2_settings['DS2'], threads, stop_event)
                     run_dus(pi2_settings['DUS2'], threads, stop_event)
                     run_pir(pi2_settings['DPIR2'], threads, stop_event)
+                    run_ds(pi2_settings['BTN'], threads, stop_event)
+                    run_dht(pi2_settings['DHT3'], threads, stop_event)
+                    run_gyro(pi2_settings['GSG'], threads, stop_event)
                     
                 if '--actuators' in args:
                     run_actuators_logic(pi1_settings)
                 else:
                     while True:
                         time.sleep(1)
+
+            
             elif args[0] == '--3':
                 print("Running in PI3 mode")
                 if '--sensors' in args:
