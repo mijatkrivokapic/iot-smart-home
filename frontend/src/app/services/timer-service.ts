22a0cb28df58f602639440d93a091bc1fe471d5a
DamjanVincic <damjanvincic2003@gmail.com>
2026-02-22 22:59:12 +0100

    chore: typo

--- frontend/src/app/services/timer-service.ts
+++ frontend/src/app/services/timer-service.ts
@@ -10,15 +10,15 @@ export class TimerService {
   constructor(private http:HttpClient) {}
 
   startTimer(duration: number): Observable<any> {
-    return this.http.post(`${environment.apiUrL}/api/timer/start`,{'time':duration});
+    return this.http.post(`${environment.apiUrl}/api/timer/start`,{'time':duration});
   }
 
   setTimerIncrement(increment: number): Observable<any> {
-    return this.http.put(`${environment.apiUrL}/api/timer/config`, { 'time' : increment });
+    return this.http.put(`${environment.apiUrl}/api/timer/config`, { 'time' : increment });
   }
 
   incrementTimer(): Observable<any> {
-    return this.http.post(`${environment.apiUrL}/api/timer/increment`, {});
+    return this.http.post(`${environment.apiUrl}/api/timer/increment`, {});
   }
 
 }
