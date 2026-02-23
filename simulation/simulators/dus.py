49150af0b5a45e79fb387008d2f45eec87023784
DamjanVincic <damjanvincic2003@gmail.com>
2026-02-23 03:33:13 +0100

    chore: change ultrasonic generator step

--- simulation/simulators/dus.py
+++ simulation/simulators/dus.py
@@ -1,14 +1,16 @@
-import time
 import random
+import time
 
 INITIAL_DISTANCE = 50
+
 
 def generate_distance():
     dist = INITIAL_DISTANCE
     while True:
-        dist = dist + random.randint(-5, 5)
+        dist = dist + random.randint(-10, 10)
         dist = max(5, min(400, dist))
         yield dist
+
 
 def run_dus_simulator(delay, callback, stop_event, sensor_config=None):
     for d in generate_distance():
